a386a1c210536305a227ba9e83657c9ceee9fe48
Nick Walraven <nickelbink@gmail.com>
2021-06-30 01:11:46 +0200

    - Added warning when trying to connect to localhost via https - Added new flag to set http or https

--- src/client.js
+++ src/client.js
@@ -4,19 +4,21 @@ const io = require('socket.io-client');
 const readline = require('readline');
 
 const options = yargs
-    .usage('Usage: -i <ip> -n <nickname> -p <port> -c <chat> -h <hex>')
-    .options('i', { alias: 'ip', describe: 'The IP-address or Domain of the socket server (with http/https)', type: 'string', demandOption: 'true' })
+    .usage('Usage: -i <ip> -n <nickname> -p <port> -c <chat> -h <hex> -s <secure>')
+    .options('i', { alias: 'ip', describe: 'The IP-address or Domain of the socket server', type: 'string', demandOption: 'true' })
     .options('n', { alias: 'nickname', describe: 'Your Nickname', type: 'string', demandOption: 'true' })
     .options('p', { alias: 'port', describe: 'The Port the socket server is running on', type: 'string' })
-    .options('c', { alias: 'chat', describe: 'The name of the Chat you are trying to join', type: 'string' })
-    .options('h', { alias: 'hex', describe: 'The HEX of the color that your name should be (standard is orange)', type: 'string' })
+    .options('c', { alias: 'chat', describe: 'The name of the Chat you are trying to join (defaults to "general")', type: 'string' })
+    .options('h', { alias: 'hex', describe: 'The HEX of the color that your name should be (defaults to #FF4500)', type: 'string' })
+    .options('s', { alias: 'insecure', describe: 'Whether the connection is to be insecure or not, http vs. https (defaults to false, https)', type: 'boolean' })
     .argv;
 
-const socket = io(`${options.ip}${options.port ? `:${options.port}` : ''}`, {
+console.log(!options.insecure && (options.ip.includes('localhost') || options.ip.includes('127.0.0.1')) ? 'You\'re securely trying to connect to a server on this machine, try running client with the -s flag (for http instead of https)' : '');
+const socket = io(`${options.insecure ? 'http://' : 'https://'}${options.ip}${options.port ? `:${options.port}` : ''}`, {
     query: {
         nickname: options.nickname,
         chat: options.chat ? options.chat : 'general',
-        color: options.hex
+        color: options.hex ? options.hex : undefined
     }
 });
 const rl = readline.createInterface({
@@ -31,6 +33,7 @@ function log(msg) {
     rl.prompt(true);
 }
 
+
 console.log(`Connecting to group [${options.chat ? options.chat : 'general'}] as [${options.nickname}]`);
 socket.on('joined', msg => {
     if (msg.success) {
@@ -43,6 +46,11 @@ socket.on('joined', msg => {
 });
 
 socket.on('message', msg => log(msg));
+
+socket.on('disconnect', msg => {
+    console.log('Connection to server lost, exiting..');
+    process.exit();
+});
 
 rl.on('line', line => {
     if (line[0] === '/' && line.length > 1) {
